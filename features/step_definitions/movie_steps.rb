073ad61f85e02edc24c99095ed8cca8930bde7e1
Sergey levandovskiy <levandovskiy.s@gmail.com>
2013-02-02 15:04:25 +0200

    prepared filter test

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,12 +15,45 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  Movie.all_ratings.each do |rate|
+    uncheck 'ratings_' + rate
+  end
+
+
+  rating_list.scan /\w+/ do |rate|
+    check 'ratings_' + rate
+  end
+
+  click_button('ratings_submit')
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+end
+
+Then /I should (not )?see movies\:/ do |exclude, list|
+  list.hashes.each do |movie|
+    step %Q{I should #{exclude}see "#{movie['title']}"}
+  end
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /(all|no) ratings selected/ do |all|
+  #handler = all ? check : uncheck
+  Movie.all_ratings.each do |rate|
+    case 'all'
+    when'all' then check 'ratings_' + rate
+    else uncheck 'ratings_' + rate
+    end
+  end
+
+  click_button('ratings_submit')
 end
